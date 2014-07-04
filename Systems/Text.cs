0ef2a68f1f1e64606a864d3ef610211696b0b00e
David Culbreth <david.culbreth.256@gmail.com>
2014-07-04 00:33:31 -0500

    Font fun
    Changed font to Monospace... for fun? I guess?

--- Systems/Text.cs
+++ Systems/Text.cs
@@ -13,7 +13,7 @@ namespace Lotus {
         QFont font;
 
         public Text() {
-            font = new QFont(new Font(FontFamily.GenericSansSerif, 16f));
+            font = new QFont(new Font(FontFamily.GenericMonospace, 16f));
         }
 
         public void Draw(string text, OpenTK.Vector2 position) {
