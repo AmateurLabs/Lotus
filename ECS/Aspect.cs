478ba00dde054e5555fef16c103ad24d258de2e2
Maroy <foemaroy@gmail.com>
2014-07-06 13:26:33 -0500

    Initial Commit

---
+++ ECS/Aspect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lotus.ECS {
+    public abstract class Aspect {
+
+        public readonly int Id;
+
+        public Aspect(int id) {
+            Id = id;
+        }
+    }
+}
---
+++ ECS/Subsystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lotus.ECS {
+    public class Subsystem {
+
+        public HashSet<int> Entities;
+        public HashSet<string> AspectMask;
+    }
+}
