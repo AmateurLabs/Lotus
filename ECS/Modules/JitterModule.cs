3158693da4eee8aee7d78e681d97865144a42350
Maroy <foemaroy@gmail.com>
2014-07-06 21:28:46 -0500

    Added ECS components

---
+++ ECS/Modules/JitterModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Jitter;
+using Jitter.Collision;
+
+namespace Lotus.ECS.Modules {
+    public class JitterModule : Module {
+
+        World world;
+        CollisionSystem collisionSystem;
+
+        public JitterModule() {
+            collisionSystem = new CollisionSystemSAP();
+            world = new World(collisionSystem);
+        }
+    }
+}
